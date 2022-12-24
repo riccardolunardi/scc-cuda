c95b5dc88d7f247eb045c323e22c3aa2ceaeb76d
Lunni <riccardo.lunardi1999@gmail.com>
2022-12-24 15:25:02 +0100

    standard deviation

--- cuda/scc_runner.cu
+++ cuda/scc_runner.cu
@@ -6,13 +6,26 @@
 #include "sccv5_openmp.cu"
 #include "sccv6_optreach.cu"
 #include <chrono>
+#include <vector>
 using namespace std;
 
 #define WARMUP 3
 
-void common_routine(void (*routine_runner)(const bool, unsigned int, unsigned int, unsigned int*, unsigned int*, unsigned int*, unsigned int*, char * ), const bool profiling, const unsigned int num_nodes, const unsigned int num_edges, unsigned int * nodes, unsigned int * adjacency_list, unsigned int * nodes_transpose, unsigned int * adjacency_list_transpose, char * status, char * og_status, const int repeat) {
+double calculateStandardDeviation(double mean, int n, double numbers[]) {
+	// Calculate the sum of squared differences
+	double sumSquaredDifferences = 0;
+	for (int i = 0; i < n; i++) {
+		double difference = numbers[i] - mean;
+		sumSquaredDifferences += difference * difference;
+	}
+
+	// Calculate the standard deviation
+	return sqrt(sumSquaredDifferences / n);
+}
+
+vector<double> common_routine(void (*routine_runner)(const bool, unsigned int, unsigned int, unsigned int*, unsigned int*, unsigned int*, unsigned int*, char * ), const bool profiling, const unsigned int num_nodes, const unsigned int num_edges, unsigned int * nodes, unsigned int * adjacency_list, unsigned int * nodes_transpose, unsigned int * adjacency_list_transpose, char * status, char * og_status, const int repeat) {
 	// Call the function passed as an argument
-	chrono::milliseconds elapsedTime(0);
+	vector<double> executionTimes;
 
 	for(short i=0; i<repeat + WARMUP; i++){
 		memcpy(status, og_status, num_nodes);
@@ -22,12 +35,34 @@ void common_routine(void (*routine_runner)(const bool, unsigned int, unsigned in
 		auto end = chrono::high_resolution_clock::now();
 
 		if (WARMUP < i) {
-			elapsedTime += chrono::duration_cast<chrono::milliseconds>(end - start);
+			executionTimes.push_back(chrono::duration<double, milli>(end - start).count());
 		}
 	}
 
-  	cout << "Total elapsed time: " << elapsedTime.count() << "ms" << endl;
-	cout << "Average elapsed time: " << elapsedTime.count()/repeat << "ms" << endl;
+	return executionTimes;
+}
+
+void print_benchmark(const vector<double> executionTimes) {
+	// Calculate the mean
+	double sum = 0;
+	for (double t : executionTimes) {
+		sum += t;
+	}
+	double mean = sum / executionTimes.size();
+
+	// Calculate the sum of squared differences
+	double sumSquaredDifferences = 0;
+	for (double t : executionTimes) {
+		double difference = t - mean;
+		sumSquaredDifferences += difference * difference;
+	}
+
+	// Calculate the standard deviation
+	double standardDeviation = sqrt(sumSquaredDifferences / executionTimes.size());
+
+  	cout << "Total elapsed time: " << sum << "ms" << endl;
+	cout << "Average elapsed time: " << mean << "ms" << endl;
+	cout << "Standard deviation: " << standardDeviation << "ms" << endl;
 }
 
 int main(unsigned int argc, char ** argv) {
@@ -46,6 +81,8 @@ int main(unsigned int argc, char ** argv) {
 	
 	printf("Lettura del file %s...\n", argv[1]);
 	create_graph_from_filename(argv[1], num_nodes, num_edges, nodes, adjacency_list, nodes_transpose, adjacency_list_transpose, og_status);
+
+	printf("Number of nodes: %d...\n", num_nodes);
 	
 	// Inizializzazione di struttrure dati per la versione 1
 	int num_nodes_v1, num_edges_v1;
@@ -74,8 +111,8 @@ int main(unsigned int argc, char ** argv) {
 	char * status;
 	status = (char *) malloc(num_nodes * sizeof(char));
 
-	printf("Versione 0 - main.cpp\n");
-	chrono::milliseconds elapsedTime(0);
+ 	printf("Versione 0 - main.cpp\n");
+	vector<double> executionTimes;
 	for(int i=0;i<repeat;i++){
 		memcpy(status, og_status, num_nodes);
 		
@@ -83,40 +120,38 @@ int main(unsigned int argc, char ** argv) {
 		routine(profiling, num_nodes, num_edges, nodes, adjacency_list, nodes_transpose, adjacency_list_transpose, status);
 		auto end = chrono::high_resolution_clock::now();
 		if (WARMUP < i) {
-			elapsedTime += chrono::duration_cast<chrono::milliseconds>(end - start);
+			executionTimes.push_back(chrono::duration<double, milli>(end - start).count());
 		}
 	}
 
-  	cout << "Total elapsed time: " << elapsedTime.count() << "ms" << endl;
-	cout << "Average elapsed time: " << elapsedTime.count()/repeat << "ms" << endl;
+  	print_benchmark(executionTimes);
 
 	printf("Versione 1 - Naive\n");
-	chrono::milliseconds elapsedTime_v1(0);
+	executionTimes.clear();
 	for(int i=0;i<repeat;i++){
 		auto start = chrono::high_resolution_clock::now();
 		routine_v1(profiling, num_nodes_v1, num_edges_v1, nodes_v1, adjacency_list_v1, nodes_transpose_v1, adjacency_list_transpose_v1, is_u);
 		auto end = chrono::high_resolution_clock::now();
 
 		if (WARMUP < i) {
-			elapsedTime_v1 += chrono::duration_cast<chrono::milliseconds>(end - start);
+			executionTimes.push_back(chrono::duration<double, milli>(end - start).count());
 		}
 	}
 
-	cout << "Total elapsed time: " << elapsedTime_v1.count() << "ms" << endl;
-	cout << "Average elapsed time: " << elapsedTime_v1.count()/repeat << "ms" << endl;
+	print_benchmark(executionTimes);
 
 	printf("Versione 2 - Status\n");
-	common_routine(routine_v2, profiling, num_nodes, num_edges, nodes, adjacency_list, nodes_transpose, adjacency_list_transpose, status, og_status, repeat);
+	print_benchmark(common_routine(routine_v2, profiling, num_nodes, num_edges, nodes, adjacency_list, nodes_transpose, adjacency_list_transpose, status, og_status, repeat));
 	
 	printf("Versione 3 - Streams\n");
-	common_routine(routine_v3, profiling, num_nodes, num_edges, nodes, adjacency_list, nodes_transpose, adjacency_list_transpose, status, og_status, repeat);
+	print_benchmark(common_routine(routine_v3, profiling, num_nodes, num_edges, nodes, adjacency_list, nodes_transpose, adjacency_list_transpose, status, og_status, repeat));
 	
 	printf("Versione 4 - Pinned\n");
-	common_routine(routine_v4, profiling, num_nodes, num_edges, nodes, adjacency_list, nodes_transpose, adjacency_list_transpose, status, og_status, repeat);
+	print_benchmark(common_routine(routine_v4, profiling, num_nodes, num_edges, nodes, adjacency_list, nodes_transpose, adjacency_list_transpose, status, og_status, repeat));
 	
 	printf("Versione 5 - OpenMP\n");
-	common_routine(routine_v5, profiling, num_nodes, num_edges, nodes, adjacency_list, nodes_transpose, adjacency_list_transpose, status, og_status, repeat);
+	print_benchmark(common_routine(routine_v5, profiling, num_nodes, num_edges, nodes, adjacency_list, nodes_transpose, adjacency_list_transpose, status, og_status, repeat));
 	
 	printf("Versione 6 - Reach Ottimizzato\n");
-	common_routine(routine_v6, profiling, num_nodes, num_edges, nodes, adjacency_list, nodes_transpose, adjacency_list_transpose, status, og_status, repeat);
+	print_benchmark(common_routine(routine_v6, profiling, num_nodes, num_edges, nodes, adjacency_list, nodes_transpose, adjacency_list_transpose, status, og_status, repeat));
 }
