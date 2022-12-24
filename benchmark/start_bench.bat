f0f60c0052c927d10da7c90ae9e2ccfcc03616b1
Lunni <riccardo.lunardi1999@gmail.com>
2022-12-24 17:23:40 +0100

    Fix percorsi

--- benchmark/start_bench.bat
+++ benchmark/start_bench.bat
@@ -1,3 +1,3 @@
 nvcc -Xcompiler /openmp -DDEBUG_FINAL=0 -DOMP_MIN_NODES=100000 .\cuda\scc_runner.cu -o "./build/scc.exe"
-echo > ./benchmarks_partial_results.txt
-for /F %%f in ('dir /B /O:N "%cd%\samples\iter_tests\sample_test_iter*"') do %cd%\build\scc.exe %cd%\samples\iter_tests\%%f 25 1 >> ./benchmarks_partial_results.txt
+echo > ./benchmark/benchmarks_partial_results.txt
+for /F %%f in ('dir /B /O:N "%cd%\samples\iter_tests\sample_test_iter*"') do %cd%\build\scc.exe %cd%\samples\iter_tests\%%f 25 1 >> ./benchmark/benchmarks_partial_results.txt
