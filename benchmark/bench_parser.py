0751912f5be8cc3675b50224ab08ca3caca5557f
Lunni <riccardo.lunardi1999@gmail.com>
2022-12-24 17:23:28 +0100

    Generalizzato punteggiatura

--- benchmark/bench_parser.py
+++ benchmark/bench_parser.py
@@ -1,8 +1,12 @@
-headers = "Nodes;Avg(v0);Std(v0);Avg(v1);Std(v1);Avg(v2);Std(v2);Avg(v3);Std(v3);Avg(v4);Std(v4);Avg(v5);Std(v5);Avg(v6);Std(v6);Avg(v7);Std(v7)"
+SEPARATOR = ";"
+SIMBOLO_INIZIO_DECIMALI = ","
+
+headers = ["Nodes","Avg(v0)","Std(v0)","Avg(v1)","Std(v1)","Avg(v2)","Std(v2)","Avg(v3)","Std(v3)","Avg(v4)","Std(v4)","Avg(v5)","Std(v5)","Avg(v6)","Std(v6)","Avg(v7)","Std(v7)"]
+headers = SEPARATOR.join(headers)
 csv_res = []
 csv_line = []
 
-with open("benchmarks_results.txt") as f:
+with open("benchmark/benchmarks_partial_results.txt") as f:
     for line in f.readlines():
         if line.startswith("Number"):
             if csv_line:
@@ -12,7 +16,7 @@ with open("benchmarks_results.txt") as f:
             csv_line.append(line.split(" ")[-1].rstrip())
         elif line.startswith("Versione"):
             bench = line.split("-")[-1].split(",")
-            bench = [format(float(b.rstrip()), ".5f").replace(".", ",") for b in bench]
+            bench = [format(float(b.rstrip()), ".5f").replace(".", f"{SIMBOLO_INIZIO_DECIMALI}") for b in bench]
             csv_line.extend(bench)
 
 with open("benchmarks_results.csv", "w") as f:
