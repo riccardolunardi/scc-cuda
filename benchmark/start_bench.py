a655fc38fa62251cdf080d4f50cce7fd880f6fda
Lunni <riccardo.lunardi1999@gmail.com>
2022-12-28 18:31:04 +0100

    Benchmark automatico

--- benchmark/start_bench.py
+++ benchmark/start_bench.py
@@ -8,17 +8,20 @@ def print_stdout(output):
     print(output.stdout.decode('utf-8').replace('\r', ''))
 
 print("Compiling CUDA code...")
-compiler_output = subprocess.run(["nvcc", "-Xcompiler", "/openmp", "-DDEBUG_FINAL=1", "-DOMP_MIN_NODES=100000", "-DWARMUP=5", ".\cuda\scc_runner.cu", "-o", "./build/scc.exe"], capture_output=True)
+compiler_output = subprocess.run(["nvcc", "-Xcompiler", "/openmp", "-DDEBUG_FINAL=0", "-DOMP_MIN_NODES=100000", "-DWARMUP=5", ".\cuda\scc_runner.cu", "-o", "./build/scc.exe"], capture_output=True)
 print_stdout(compiler_output)
 
 FOLDER_PATH = 'F:/network-benchmark/final/'
 
 files = os.listdir(FOLDER_PATH)
+rects = []
+fig, ax = plt.subplots()
+
 for file in files:
-    if file.endswith('.txt') and file.startswith('twitter'): # or file.startswith('amazon')
+    if file.endswith('.txt') and file.startswith('twitter.'): # or file.startswith('amazon')
         print(f"Benchmarking {file}...")
         
-        output = subprocess.run(['./build/scc.exe', FOLDER_PATH + file, '5', '0'], capture_output=True)
+        output = subprocess.run(['./build/scc.exe', FOLDER_PATH + file, '50', '1'], capture_output=True)
         print_stdout(output)
         output_string = output.stdout.decode('utf-8')
 
@@ -31,36 +34,22 @@ for file in files:
             avg.append(float(avg_run))
             std.append(float(std_run))
 
-        versions_runned = re.findall(r"Versione \d", output_string)
+        versions_runned = re.findall(r"Versione \d -(.+)-", output_string)
         
         print(versions_runned)
         print(avg)
         print(std)
 
         x = np.arange(len(versions_runned))  # the label locations
-        width = 0.35  # the width of the bars
     
-        plt.bar(versions_runned, avg, label='Values 1')
-        plt.bar(versions_runned, std, bottom=avg, label='Values 2')
-        fig, ax = plt.subplots()
-        rects1 = ax.bar(x - width/2, avg, width, label='Values 1', yerr=std, error_kw=dict(lw=5, capsize=5, capthick=3))
+        plt.bar(versions_runned, avg, yerr=std, error_kw=dict(lw=5, capsize=5, capthick=3))
 
         # Add labels and title
-        ax.set_xlabel('Versions')
-        ax.set_ylabel('Values')
-        ax.set_title('Bar Plot Example')
+        ax.set_xlabel('Versione', labelpad=-3)
+        ax.set_ylabel('Time (ms)')
+        ax.set_title(graph_names[0].capitalize(), fontdict={'fontsize': 20, 'fontweight': 'heavy'}, pad=15)
         ax.set_xticks(x)
-        ax.set_xticklabels(versions_runned)
-
-        # Add the legend
-        ax.legend()
+        ax.set_xticklabels([v.replace(" ", "\n") for v in versions_runned], fontsize=8)
 
         # Show the plot
-        plt.show()
-
-        data_run = zip(versions_runned, avg, std)
-
-        for a,b,c in data_run:
-            print(f"{a}, {b}, {c}")
-        
-        break
+        plt.savefig(f'./benchmark/{graph_names[0]}.png')
