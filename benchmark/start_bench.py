d582ed9c078b4f724ca5299dd34a6fb3ec66b8e6
Riccardo <riccardo.lunardi1999@gmail.com>
2022-12-28 21:10:30 +0100

    Fix parametri

--- benchmark/start_bench.py
+++ benchmark/start_bench.py
@@ -8,21 +8,19 @@ def print_stdout(output):
     print(output.stdout.decode('utf-8').replace('\r', ''))
 
 print("Compiling CUDA code...")
-compiler_output = subprocess.run(["nvcc", "-Xcompiler", "/openmp", "-DDEBUG_FINAL=0", "-DOMP_MIN_NODES=100000", "-DWARMUP=1", ".\cuda\scc_runner.cu", "-o", "./build/scc.exe"], capture_output=True)
+compiler_output = subprocess.run(["nvcc", "-Xcompiler", "/openmp", "-DDEBUG_FINAL=0", "-DOMP_MIN_NODES=100000", "-DWARMUP=5", ".\cuda\scc_runner.cu", "-o", "./build/scc.exe"], capture_output=True)
 print_stdout(compiler_output)
 
-FOLDER_PATH = 'F:/network-benchmark/final/'
+FOLDER_PATH = 'C:/Users/rikyn/Desktop/final/'
 
 files = os.listdir(FOLDER_PATH)
-rects = []
-fig, ax = plt.subplots()
 
 for file in files:
     if file.endswith('.txt'):
         print(f"Benchmarking {file}...")
         
         try:
-            output = subprocess.run(['./build/scc.exe', FOLDER_PATH + file, '50', '1'], capture_output=True)
+            output = subprocess.run(['./build/scc.exe', FOLDER_PATH + file, '25', '1'], capture_output=True)
             print_stdout(output)
             output_string = output.stdout.decode('utf-8')
 
@@ -46,18 +44,20 @@ for file in files:
                 for i in range(len(versions_runned)):
                     f.write(f"{versions_runned[i]}\t{avg[i]}\t{std[i]}\n")
 
-            x = np.arange(len(versions_runned))  # the label locations
-        
-            plt.bar(versions_runned, avg, yerr=std, error_kw=dict(lw=5, capsize=5, capthick=3))
+            for i in range(0,2):
+                plt.figure()
+                fig, ax = plt.subplots()
 
-            # Add labels and title
-            ax.set_xlabel('Versione', labelpad=-3)
-            ax.set_ylabel('Time (ms)')
-            ax.set_title(graph_names[0].capitalize(), fontdict={'fontsize': 20, 'fontweight': 'heavy'}, pad=15)
-            ax.set_xticks(x)
-            ax.set_xticklabels([v.replace(" ", "\n") for v in versions_runned], fontsize=8)
+                plt.bar(versions_runned[i:], avg[i:], yerr=std[i:], error_kw=dict(lw=5, capsize=5, capthick=3))
 
-            # Show the plot
-            plt.savefig(f'./benchmark/result/{graph_names[0]}.png')
+                # Add labels and title
+                ax.set_xlabel('Versione', labelpad=-3)
+                ax.set_ylabel('Time (ms)')
+                ax.set_title(graph_names[0].capitalize(), fontdict={'fontsize': 20, 'fontweight': 'heavy'}, pad=15)
+                ax.set_xticks(np.arange(len(versions_runned[i:])))
+                ax.set_xticklabels([v.replace(" ", "\n") for v in versions_runned[i:]], fontsize=8)
+
+                # Show the plot
+                plt.savefig(f'./benchmark/result/{graph_names[0]}_{i}.png')
         except Exception as e:
             print("Error: ", e, " - skipping file...")
